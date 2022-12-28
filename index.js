c7471f4c735cb0c67c91c2b80aea99b538da5b49
kerasus <aliesmaeeli20@gmail.com>
2022-12-28 12:19:33 +0330

    Allow use of default import syntax in TypeScript

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
-import QuasarTemplateBuilder from './src/quasar-template-builder'
-import QuasarTemplateBuilderAppLayout from './src/store/QuasarTemplateBuilderAppLayout'
-import Breadcrumbs from './src/components/breadcrumbs';
-import {getRouteWithParent, createBreadcrumbsFromRouteWithParent, createLayoutConfigArray} from './src/helper'
+const QTemplateBuilderHelper = require('./src/helper')
+const Breadcrumbs = require('./src/components/breadcrumbs')
+const QuasarTemplateBuilder = require('./src/quasar-template-builder')
+const QuasarTemplateBuilderAppLayout = require('./src/store/QuasarTemplateBuilderAppLayout')
+const {getRouteWithParent, createBreadcrumbsFromRouteWithParent, createLayoutConfigArray} = QTemplateBuilderHelper
+const QuasarTemplateBuilderObject = { QuasarTemplateBuilder, QuasarTemplateBuilderAppLayout, Breadcrumbs,  getRouteWithParent, createBreadcrumbsFromRouteWithParent, createLayoutConfigArray }
 
-export { QuasarTemplateBuilder, QuasarTemplateBuilderAppLayout, Breadcrumbs,  getRouteWithParent, createBreadcrumbsFromRouteWithParent, createLayoutConfigArray }
+module.exports = QuasarTemplateBuilderObject
+module.exports.default = QuasarTemplateBuilderObject
