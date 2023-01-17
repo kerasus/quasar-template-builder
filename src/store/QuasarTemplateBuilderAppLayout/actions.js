323275b2e8f7b82b276a95e80775c8846b1d30a1
kerasus <aliesmaeeli20@gmail.com>
2023-01-17 15:05:30 +0330

    updateStore prevent to check mutation exist

--- src/store/QuasarTemplateBuilderAppLayout/actions.js
+++ src/store/QuasarTemplateBuilderAppLayout/actions.js
@@ -1,13 +1,13 @@
-const mutations = require('./mutations')
+// const mutations = require('./mutations')
 
 module.exports = {
   updateStore: (context, data) => {
     let mutation = ''
     Object.keys(data).forEach(key => {
       mutation = 'update' + key.substring(0, 1).toUpperCase() + key.substring(1)
-      if (mutations[mutation]) {
+      // if (mutations[mutation]) {
         context.commit(mutation, data[key])
-      }
+      // }
     })
   }
 }
