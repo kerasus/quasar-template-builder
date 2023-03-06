a2566654b7b11c5e0fb392c7a483de7660309cf9
kerasus <aliesmaeeli20@gmail.com>
2023-03-06 12:45:58 +0330

    change index export

--- src/store/QuasarTemplateBuilderAppLayout/index.js
+++ src/store/QuasarTemplateBuilderAppLayout/index.js
@@ -1,9 +1,9 @@
 import state from './state'
-import * as getters from './getters'
-import * as mutations from './mutations'
-import * as actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
 
-module.exports = {
+export default {
   namespaced: true,
   getters,
   mutations,
