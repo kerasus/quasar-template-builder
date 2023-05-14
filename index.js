77ad49e1494f65e5438ea2885360973eae1a3dec
kerasus <aliesmaeeli20@gmail.com>
2023-05-14 12:37:31 +0330

    update export

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const QTemplateBuilderHelper = require('./src/helper')
-const Breadcrumbs = require('./src/components/breadcrumbs')
-const QuasarTemplateBuilder = require('./src/quasar-template-builder')
+const QTemplateBuilderHelper = require('./src/helper.js')
+const Breadcrumbs = require('./src/components/breadcrumbs.vue')
+const QuasarTemplateBuilder = require('./src/quasar-template-builder.vue')
 const QuasarTemplateBuilderAppLayout = require('./src/store/QuasarTemplateBuilderAppLayout')
 const {getRouteWithParent, createBreadcrumbsFromRouteWithParent, createLayoutConfigArray} = QTemplateBuilderHelper
 const QuasarTemplateBuilderObject = { QuasarTemplateBuilder, QuasarTemplateBuilderAppLayout, Breadcrumbs,  getRouteWithParent, createBreadcrumbsFromRouteWithParent, createLayoutConfigArray }
